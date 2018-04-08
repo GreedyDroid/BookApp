5916b83545cdc775154ab1d5d21f711152860cd8
sayed <nuruddinsayeed@gmail.com>
2018-04-09 04:47:16 +0600

    Initial commit

---
+++ app/src/main/java/com/bookappproject/sayed/bookapp/Home.java
@@ -0,0 +1,17 @@
+package com.bookappproject.sayed.bookapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
