7a8fedc45ec3a8cb1d93dea4d855f7c94b336765
sayed <nuruddinsayeed@gmail.com>
2018-04-13 09:22:06 +0600

    Book Database Bug Fixed

--- app/src/main/java/com/bookappproject/sayed/bookapp/book_detail/BookDetail.java
+++ app/src/main/java/com/bookappproject/sayed/bookapp/book_detail/BookDetail.java
@@ -4,19 +4,25 @@ import java.util.ArrayList;
 
 public class BookDetail {
 
-    private String bookID;
+    private String bookName;
+    private String bookFirebaseID;
     private ArrayList<Chapter>chapters;
 
-    public BookDetail(String bookID, ArrayList<Chapter> chapters) {
-        this.bookID = bookID;
+    public BookDetail(String bookName, String bookFirebaseID, ArrayList<Chapter> chapters) {
+        this.bookName = bookName;
+        this.bookFirebaseID = bookFirebaseID;
         this.chapters = chapters;
     }
 
     public BookDetail() {
     }
 
-    public String getBookID() {
-        return bookID;
+    public String getBookName() {
+        return bookName;
+    }
+
+    public String getBookFirebaseID() {
+        return bookFirebaseID;
     }
 
     public ArrayList<Chapter> getChapters() {
--- app/src/main/java/com/bookappproject/sayed/bookapp/book_detail/book_detail_mobile_database/BookDetailDatabaseSource.java
+++ app/src/main/java/com/bookappproject/sayed/bookapp/book_detail/book_detail_mobile_database/BookDetailDatabaseSource.java
@@ -1,14 +1,13 @@
 package com.bookappproject.sayed.bookapp.book_detail.book_detail_mobile_database;
 
 import android.content.ContentValues;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.bookappproject.sayed.bookapp.book_detail.BookDetail;
 import com.bookappproject.sayed.bookapp.book_detail.Chapter;
-import com.bookappproject.sayed.bookapp.main_books_database.AllBooksDatabaseHelper;
-import com.bookappproject.sayed.bookapp.main_books_database.MainBookObject;
 
 import java.util.ArrayList;
 
@@ -18,31 +17,43 @@ public class BookDetailDatabaseSource {
     private BookDetailDatabaseHelper databaseHelper;
     private SQLiteDatabase database;
 
-    public BookDetailDatabaseSource(Context context, String bookNameTbl) {
-        databaseHelper = new BookDetailDatabaseHelper(context, bookNameTbl);
+    public BookDetailDatabaseSource(Context context) {
+        databaseHelper = new BookDetailDatabaseHelper(context);
     }
+
 
     public void open(){
         database = databaseHelper.getWritableDatabase();
+/*        int version= database.getVersion();
+        database.needUpgrade(version+1);*/
+
     }
+    public void openRedable(){
+        database = databaseHelper.getReadableDatabase();
+    }
+
     public void close(){
         database.close();
     }
 
+
+
     public boolean addBook(BookDetail bookDetail){
-        String bookNameTbl = bookDetail.getBookID();
-        this.open();
-        String alu = "alu";
+        String bookNameTbl = bookDetail.getBookFirebaseID();
+        open();
+
         ContentValues contentValues;
         boolean status = true;
         for (Chapter chapter:bookDetail.getChapters()) {
             contentValues = new ContentValues();
-            contentValues.put(BookDetailDatabaseHelper.COL_FIREBASE_ID, chapter.getFirebaseID());
-            contentValues.put(BookDetailDatabaseHelper.COL_SERIAL, chapter.getChapterSerial());
-            contentValues.put(BookDetailDatabaseHelper.COL_TITLE, chapter.getChapterTitle());
-            contentValues.put(BookDetailDatabaseHelper.COL_DESCRIPTION, chapter.getChapterDescription());
+            contentValues.put(BookDetailDatabaseHelper.COL_FIREBASE_BOOK_ID, bookDetail.getBookFirebaseID());
+            contentValues.put(BookDetailDatabaseHelper.COL_BOOK_NAME, bookDetail.getBookName());
+            contentValues.put(BookDetailDatabaseHelper.COL_CHAPTER_FIREBASE_ID, chapter.getFirebaseID());
+            contentValues.put(BookDetailDatabaseHelper.COL_CHAPTER_SERIAL, chapter.getChapterSerial());
+            contentValues.put(BookDetailDatabaseHelper.COL_CHAPTER_TITLE, chapter.getChapterTitle());
+            contentValues.put(BookDetailDatabaseHelper.COL_CHAPTER_DESCRIPTION, chapter.getChapterDescription());
 
-            long row_id = database.insert(bookNameTbl, null, contentValues);
+            long row_id = database.insert(BookDetailDatabaseHelper.TABLE_NAME, null, contentValues);
             if (!(row_id>0)){
                 status = false;
             }
@@ -53,28 +64,35 @@ public class BookDetailDatabaseSource {
 
 
     //Get All Chapter
-    public ArrayList<Chapter> getAllChapter(String bookNameTbl){
+    public BookDetail getBookDetail(String bookFirebaseID){
         ArrayList<Chapter>chapters =  new ArrayList<>();
-        this.open();
+        this.openRedable();
 
-        Cursor cursor = database.query(bookNameTbl, null, null, null,
+        Cursor cursor = database.query(BookDetailDatabaseHelper.TABLE_NAME, null, BookDetailDatabaseHelper.COL_FIREBASE_BOOK_ID+" = ?",
+                                        new String[]{bookFirebaseID},
                                         null, null, null);
-
+        boolean flag = true;
+        String bookName="";
         if (cursor.moveToFirst()){
             do {
+                if(flag){
+                    bookName = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_BOOK_NAME));
+                    flag = false;
+                }
                 int id = cursor.getInt(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_ID));
-                String firebaseID = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_FIREBASE_ID));
-                String chapterSerial = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_SERIAL));
-                String chapterTitle = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_TITLE));
-                String chapterDescription = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_DESCRIPTION));
+                String firebaseChapterID = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_CHAPTER_FIREBASE_ID));
+                String chapterSerial = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_CHAPTER_SERIAL));
+                String chapterTitle = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_CHAPTER_TITLE));
+                String chapterDescription = cursor.getString(cursor.getColumnIndex(BookDetailDatabaseHelper.COL_CHAPTER_DESCRIPTION));
 
-                Chapter chapter = new Chapter(id, firebaseID, chapterSerial, chapterTitle, chapterDescription);
+                Chapter chapter = new Chapter(id, firebaseChapterID, chapterSerial, chapterTitle, chapterDescription);
                 chapters.add(chapter);
             }while (cursor.moveToNext());
         }
         cursor.close();
         this.close();
-        return chapters;
+
+        return new BookDetail(bookName, bookFirebaseID, chapters);
     }
 
     public boolean deleteChapter(int rowId, String bookNameTbl){
@@ -90,16 +108,16 @@ public class BookDetailDatabaseSource {
     }
 
     //TODO: Is there any error???
-    public boolean deleteBook(String bookNameTbl){
+    public boolean deleteBook(){
         this.open();
-        int deleted = database.delete(bookNameTbl,null,null);
+        int deleted = database.delete(BookDetailDatabaseHelper.TABLE_NAME,null,null);
         this.close();
         if (deleted>0){
             return true;
         }else return false;
     }
 
-    //update single Chapter
+ /*   //update single Chapter
     public boolean updateChapter(String bookNameTbl, Chapter chapter, int rowID) {
         this.open();
         ContentValues contentValues = new ContentValues();
@@ -117,5 +135,5 @@ public class BookDetailDatabaseSource {
         }else {
             return false;
         }
-    }
+    }*/
 }
